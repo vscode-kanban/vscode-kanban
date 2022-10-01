52d9df8a48c9443576d7939aac5a549e3ec4d27f
Marcel Kloubert <marcel.kloubert@gmx.net>
2022-10-01 23:16:39 +0200

    minor changes

--- media/components/board.jsx
+++ media/components/board.jsx
@@ -1,5 +1,22 @@
 /* eslint-disable @typescript-eslint/naming-convention */
 
+/**
+ * This file is part of the vscode-kanban distribution.
+ * Copyright (c) Marcel Joachim Kloubert.
+ *
+ * vscode-kanban is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, version 3.
+ *
+ * vscode-kanban is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 const KanbanBoard = () => {
   // const [now, setNow] = React.useState("");
 
