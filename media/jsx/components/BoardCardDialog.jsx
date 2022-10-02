725e42b3cd732ef418ed9ce338d728a3e9c5f883
Marcel Kloubert <marcel.kloubert@gmx.net>
2022-10-02 22:12:47 +0200

    continue implementation of BoardCardDialog component

--- media/jsx/components/BoardCardDialog.jsx
+++ media/jsx/components/BoardCardDialog.jsx
@@ -24,17 +24,46 @@
     onClose,
     show
   }) => {
-    const [Modal, Button] = window.vscodeKanban.getBootstrapComponents('Modal', 'Button');
+    const [Modal, Button, Form, Row, Col] = window.vscodeKanban.getBootstrapComponents(
+      'Modal', 'Button', 'Form', 'Row', 'Col'
+    );
+
+    const [assignedTo, setAssignedTo] = React.useState('');
+    const [category, setCategory] = React.useState('');
+    const [title, setTitle] = React.useState('');
+    const [type, setType] = React.useState('');
 
     const handleClose = React.useCallback(() => {
       onClose();
     }, [onClose]);
 
+    const headerClasses = React.useMemo(() => {  
+      const classes = [];
+      if (['bug'].includes(type)) {
+        classes.push('bg-dark', 'text-light');
+      } else if (['emergency'].includes(type)) {
+        classes.push('bg-danger', 'text-light');
+      } else {
+        classes.push('bg-info', 'text-dark');
+      }
+  
+      return classes;
+    }, [type]);
+
     const renderTitle = React.useCallback(() => {
       if (mode === 'edit') {
-        return `Edit card '${card.title}'`;
+        return `Edit card '${title}'`;
       }
-    }, [card, mode]);
+    }, [mode, title]);
+
+    React.useEffect(() => {
+      if (mode === 'edit') {
+        setTitle(String(card.title || ''));
+        setType(String(card.type || '').toLowerCase().trim());
+        setCategory(String(card.category || ''));
+        setAssignedTo(String(card.assignedTo?.name || ''));
+      }
+    }, []);
   
     return (
       show && (
@@ -45,10 +74,71 @@
           keyboard={false}
           centered
         >
-          <Modal.Header closeButton>
+          <Modal.Header
+            closeButton
+            className={headerClasses.join(' ')}
+          >
             <Modal.Title>{renderTitle()}</Modal.Title>
           </Modal.Header>
-          <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+          <Modal.Body>
+            <Form>
+              {/* title */}
+              <Row className="mb-3">
+                <Col>
+                  <Form.Label>Title</Form.Label>
+                  <Form.Control
+                    type="text"
+                    value={title}
+                    onChange={(ev) => { setTitle(ev.target.value); }}
+                  />
+                </Col>
+              </Row>
+
+              {/* type + prio */}
+              <Row className="mb-3">
+                <Col xs={8}>
+                  <Form.Label>Type</Form.Label>
+                  <Form.Select
+                    value={type}
+                    onChange={(ev) => { setType(ev.target.value); }}
+                  >
+                    <option value="">Note / task</option>
+                    <option value="bug">Bug / issue</option>
+                    <option value="emergency">Emergency</option>
+                  </Form.Select>
+                </Col>
+
+                <Col>
+                <Form.Label>Prio</Form.Label>
+                  <Form.Control type="number" placeholder="0" />
+                </Col>
+              </Row>
+
+              {/* category */}
+              <Row className="mb-3">
+                <Col>
+                  <Form.Label>Category</Form.Label>
+                  <Form.Control
+                    type="text"
+                    value={category}
+                    onChange={(ev) => { setCategory(ev.target.value); }}
+                  />
+                </Col>
+              </Row>
+
+              {/* assigned to */}
+              <Row className="mb-3">
+                <Col>
+                  <Form.Label>Assigned To</Form.Label>
+                  <Form.Control
+                    type="text"
+                    value={assignedTo}
+                    onChange={(ev) => { setAssignedTo(ev.target.value); }}
+                  />
+                </Col>
+              </Row>
+            </Form>
+          </Modal.Body>
           <Modal.Footer>
             <Button variant="primary" onClick={handleClose}>
               Save Changes
